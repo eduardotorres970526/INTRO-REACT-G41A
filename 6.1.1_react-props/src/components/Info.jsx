fbec0d13cb04c94fce580040d410251122e21d99
eduardo_torres <teduardofelipe@gmail.com>
2025-07-12 15:21:57 -0500

    6.1.1_react-props

---
+++ 6.1.1_react-props/src/components/Info.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Info = ({ title, desc }) => {
+  return (
+    <section className="info">
+       <div className="contenedor">
+           <h2 className="titulo left">{title}</h2>
+           <p>{desc}</p>
+      </div>
+  </section>
+  )
+}
+
+export default Info
